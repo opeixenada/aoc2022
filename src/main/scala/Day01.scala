498ba1773ebbc5c479a4a3e08b288ef4898c495a
Anna Yudina <annayudi@gmail.com>
2022-12-01 10:26:17 +0100

    Day 1

--- src/main/scala/Day01.scala
+++ src/main/scala/Day01.scala
@@ -3,5 +3,26 @@ import Util.readFile
 import scala.io.Source
 
 @main def day01(): Unit = {
-  println("Hello world!")
+
+  val input = readFile("resources/day01")
+
+  // Part 1
+
+  val (maxCalories, _) = input.foldLeft((0, 0)) {
+    case ((currentMax, currentSum), s) =>
+      if (s.isBlank) (Math.max(currentSum, currentMax), 0)
+      else (currentMax, currentSum + s.toInt)
+  }
+
+  println(maxCalories)
+
+  // Part 2
+
+  val weights = input.foldLeft(List(0)) {
+    case (x :: xs, s) =>
+      if (s.isBlank) 0 :: x :: xs
+      else (x + s.toInt) :: xs
+  }
+
+  println(weights.sorted.takeRight(3).sum)
 }
